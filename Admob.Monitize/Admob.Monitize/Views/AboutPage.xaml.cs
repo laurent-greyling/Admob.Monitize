5415d3b24ea05749e89110d022278745abe65526
laurent.greyling@gmail.com <laurent.greyling@gmail.com>
2017-12-06 10:50:41 +0100

    Add project files.

---
+++ Admob.Monitize/Admob.Monitize/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Admob.Monitize
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
