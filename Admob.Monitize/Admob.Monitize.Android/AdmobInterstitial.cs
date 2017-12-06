e3be036704ecfd6c0b4b0796bf20d19bba90aabc
laurent.greyling@gmail.com <laurent.greyling@gmail.com>
2017-12-06 15:08:42 +0100

    Implement show

---
+++ Admob.Monitize/Admob.Monitize.Android/AdmobInterstitial.cs
@@ -0,0 +1,27 @@
+﻿using Admob.Monitize.Controls;
+using Admob.Monitize.Droid;
+using Android.Gms.Ads;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AdmobInterstitial))]
+namespace Admob.Monitize.Droid
+{
+    public class AdmobInterstitial : IAdmobInterstitial
+    {
+        InterstitialAd _ad;
+
+        public void Show(string adUnit)
+        {
+            _ad = new InterstitialAd(Forms.Context)
+            {
+                AdUnitId = adUnit
+            };
+
+            var intlistener = new InterstitialAdListener(_ad);
+            intlistener.OnAdLoaded();
+            _ad.AdListener = intlistener;
+
+           _ad.LoadAd(new AdRequest.Builder().Build());
+        }
+    }
+}
