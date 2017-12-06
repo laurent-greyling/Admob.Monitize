67f1f58bd6d1ebd5fac28dca3c75119a1a3246bb
laurent.greyling@gmail.com <laurent.greyling@gmail.com>
2017-12-06 15:03:03 +0100

    Ad listener

---
+++ Admob.Monitize/Admob.Monitize.Android/InterstitialAdListener.cs
@@ -0,0 +1,22 @@
+﻿using Android.Gms.Ads;
+
+namespace Admob.Monitize.Droid
+{
+    public class InterstitialAdListener : AdListener
+    {
+        readonly InterstitialAd _ad;
+
+        public InterstitialAdListener(InterstitialAd ad)
+        {
+            _ad = ad;
+        }
+
+        public override void OnAdLoaded()
+        {
+            base.OnAdLoaded();
+
+            if (_ad.IsLoaded)
+                _ad.Show();
+        }
+    }
+}
